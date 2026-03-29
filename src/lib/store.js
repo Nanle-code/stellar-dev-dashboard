2fdcc66b37832c03ceb9dbc74f500ed2567530bf
pope-h <hamatthan@gmail.com>
2026-03-29 20:20:20 +0100

    feat: notification system (fixes #46)

--- src/lib/store.js
+++ src/lib/store.js
@@ -200,4 +200,13 @@ export const useStore = create(persistMiddleware((set, get) => ({
     walletType: null,
     walletPublicKey: null,
   }),
+
+  // Notifications
+  notifications: [],
+  addNotification: (notification) => set((state) => ({
+    notifications: [...state.notifications, notification]
+  })),
+  removeNotification: (id) => set((state) => ({
+    notifications: state.notifications.filter(n => n.id !== id)
+  })),
 })))
